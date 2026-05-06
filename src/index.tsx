d697967f4a649d1400dd486b95aa62872dd3c4ff
HrushikeshJoshi187 <hrushikesh.joshi.187@gmail.com>
2026-05-06 01:12:36 -0500

    config

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,14 @@ import "./index.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
-  console.error(
-    "Root element not found. Ensure there is an element with id 'root' in your HTML."
-  );
+  console.error("Root element not found. Ensure there is an element with id 'root' in your HTML.");
   throw new Error("Application cannot be rendered without root element.");
 }
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/FlavorCharterInfo">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
